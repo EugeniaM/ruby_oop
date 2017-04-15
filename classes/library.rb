b0d64c362169b18fa7046007970dc89feb930295
EugeniaM <maltsevae88@gmail.com>
2017-04-15 20:35:56 +0300

    Changed class veriables to instance variables in Library class

--- classes/library.rb
+++ classes/library.rb
@@ -1,8 +1,5 @@
 module OopLibrary
 	class Library
-
-		@@ordered_books_count = {}
-		@@readers_count = {}
 		
 		def initialize(books, orders, readers, authors)
 			@books = books
@@ -10,23 +7,26 @@ module OopLibrary
 			@readers = readers
 			@authors = authors
 
+			@ordered_books_count = {}
+			@readers_count = {}
+
 			@orders.each do |order|
 				addToStatistics(order)
 			end
 		end
 
 		def getTopReader
-			top_readers = getTop(@@readers_count, 1)
+			top_readers = getTop(@readers_count, 1)
 			"The top reader is: #{top_readers.join(', ')}"
 		end
 
 		def getTopBook
-			top_books = getTop(@@ordered_books_count, 1)
+			top_books = getTop(@ordered_books_count, 1)
 			"The top book is: #{top_books.join(', ')}"
 		end
 
 		def countReadersMostPopular
-			top_three_books = getTop(@@ordered_books_count, 3)
+			top_three_books = getTop(@ordered_books_count, 3)
 			count_readers = @orders.map { |order| order.reader if top_three_books.include? (order.book)}.compact.uniq.length
 
 			"The number of people ordered one of the three most popular books: #{count_readers}"
@@ -44,13 +44,13 @@ module OopLibrary
 		end
 
 		def addToStatistics(order)
-			(@@ordered_books_count.key? (order.book)) ? 
-				(@@ordered_books_count[order.book] += 1) : 
-				(@@ordered_books_count[order.book] = 1)
+			(@ordered_books_count.key? (order.book)) ? 
+				(@ordered_books_count[order.book] += 1) : 
+				(@ordered_books_count[order.book] = 1)
 
-			(@@readers_count.key? (order.reader)) ? 
-				(@@readers_count[order.reader] += 1) : 
-				(@@readers_count[order.reader] = 1)
+			(@readers_count.key? (order.reader)) ? 
+				(@readers_count[order.reader] += 1) : 
+				(@readers_count[order.reader] = 1)
 		end
 	end
 end
